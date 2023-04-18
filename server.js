be4e9bda8429e746ef5943bfcc91ceccac0b2211
priscilla-licup <113849229+priscilla-licup@users.noreply.github.com>
2023-04-18 22:11:08 +0800

    update and delete from node 2 and 3

--- server.js
+++ server.js
@@ -7,22 +7,22 @@ const app = express();
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'rCo100928',
-    database: 'node1'
+    password: '12345678', // rCo100928
+    database: 'central_node'
 });
 
 const node2 = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'rCo100928',
-    database: 'node1'
+    password: '12345678',
+    database: 'node2'
 });
 
 const node3 = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'rCo100928',
-    database: 'node1'
+    password: '12345678',
+    database: 'node3'
 });
 
 
@@ -34,14 +34,59 @@ db.connect(function(err) {
     console.log('connected as id' + db.threadId);
 });
 
+//connect other node
+node2.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected as id' + node2.threadId);
+});
+
+//connect other node
+node3.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected as id' + node3.threadId);
+});
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// viewing central node
 app.get('/',(req, res) => {
     // res.send('CRUD Operation using NodeJS / ExpressJS / MySQL');
     let sql = "SELECT * FROM movies LIMIT 1000";
     let query = db.query(sql, (err, rows) => {
+        if(err) throw err;
+        res.render('index', {
+            title : 'Movies Database',
+            movies : rows
+        });
+    });
+});
+
+//viewing node2
+app.get('/node2',(req, res) => {
+    // res.send('CRUD Operation using NodeJS / ExpressJS / MySQL');
+    let sql = "SELECT * FROM movies LIMIT 1000";
+    let query = node2.query(sql, (err, rows) => {
+        if(err) throw err;
+        res.render('index', {
+            title : 'Movies Database',
+            movies : rows
+        });
+    });
+});
+
+//viewing node3
+app.get('/node3',(req, res) => {
+    // res.send('CRUD Operation using NodeJS / ExpressJS / MySQL');
+    let sql = "SELECT * FROM movies LIMIT 1000";
+    let query = node3.query(sql, (err, rows) => {
         if(err) throw err;
         res.render('index', {
             title : 'Movies Database',
@@ -89,7 +134,7 @@ app.post('/update',(req, res) => {
     let sql = "update movies SET name='"+req.body.name+"',  year='"+req.body.year+"',  `rank`='"+req.body.rank+"' where id ="+movieId;
     
     // Set the transaction isolation level to serializable
-    db.query('SET TRANSACTION ISOLATION LEVEL SERIALIZABLE');
+    db.query('SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED');
 
     // Start a transaction
     db.beginTransaction((err) => {
@@ -104,6 +149,13 @@ app.post('/update',(req, res) => {
         }
         console.log('Affected rows:', result.affectedRows);
 
+        // NOT SURE if updating node 2 and 3 should be done during or after transaction
+        if (req.body.year < 1980) {
+            node2.query(sql,(err, results) => {if(err) throw err;});
+        } else if (req.body.year >= 1980) {
+            node3.query(sql,(err, results) => {if(err) throw err;});
+        }
+
         // Commit the transaction
         db.commit((err) => {
         if (err) {
@@ -111,6 +163,8 @@ app.post('/update',(req, res) => {
             throw err;
             });
         }
+
+        
         res.redirect('/');
         console.log('Transaction committed');
 
@@ -124,37 +178,77 @@ app.post('/update',(req, res) => {
     // });
 });
 
+// app.get('/delete/:movieId',(req, res) => {
+//     const movieId = req.params.movieId;
+
+//     // Set the transaction isolation level to serializable
+//     db.query('SET TRANSACTION ISOLATION LEVEL SERIALIZABLE');
+
+//     db.beginTransaction((err) => {
+//         if (err) {throw err;}
+
+        
+//         //Execute SQL statements within the transaction
+//         db.query('DELETE from movies where id = ?', [movieId], (err, result) => {
+//             if (err) {
+//                 connection.rollback(() => {
+//                     throw err;
+//                 });
+//             }
+//             console.log('Affected rows:', result.affectedRows);
+
+//             //commit the transaction
+//             db.commit((err) => {
+//                 if (err) {
+//                     connection.rollback(() => {
+//                         throw err;
+//                     });
+//                 }
+//                 res.redirect('/');
+//                 console.log('Transaction committed');
+ 
+//             });
+//         });
+//     });
+// });
+
 app.get('/delete/:movieId',(req, res) => {
     const movieId = req.params.movieId;
+    //let sql = `DELETE from users where id = ${userId}`;
 
     // Set the transaction isolation level to serializable
     db.query('SET TRANSACTION ISOLATION LEVEL SERIALIZABLE');
 
     db.beginTransaction((err) => {
-        if (err) {throw err;}
 
-        //Execute SQL statements within the transaction
-        db.query('DELETE from movies where id = ?', [movieId], (err, result) => {
+    let temp_query = central_node.query('Select * from movies where id = ?',[movieId], (err, result) => {
+        let movie_year = result[0].year;
+        let delete_query = "DELETE from movies where id ="+movieId;
+
+        let query = central_node.query(delete_query,(err, result) => {
+            if(err) throw err;
+        });
+
+        if (movie_year < 1980) {
+            node2.query(delete_query,(err, results) => {if(err) throw err;});
+        } else if (movie_year >= 1980) {
+            node3.query(delete_query,(err, results) => {if(err) throw err;});
+        }
+
+        //commit the transaction
+        db.commit((err) => {
             if (err) {
                 connection.rollback(() => {
                     throw err;
                 });
             }
+            res.redirect('/');
             console.log('Affected rows:', result.affectedRows);
+            console.log('Transaction committed');
 
-            //commit the transaction
-            db.commit((err) => {
-                if (err) {
-                    connection.rollback(() => {
-                        throw err;
-                    });
-                }
-                res.redirect('/');
-                console.log('Transaction committed');
- 
-            });
         });
     });
+});
 });
 
 app.listen(3000, function () {
